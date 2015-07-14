2a1917683df16e4f9a3e6341824af18d17e8c808
dongxianhe <690515536@qq.com>
2015-07-14 16:56:37 +0800

    Initial commit

---
+++ app/src/main/java/com/brcorner/aviary/MainApplication.java
@@ -0,0 +1,11 @@
+package com.brcorner.aviary;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
